f856226363e1cf8639ab5b715efe65fb71487358
LAPTOP-E31FCVM6\sibbr <sibbreakers@gmail.com>
2022-06-30 03:36:56 +0800

    Connect database. Create function add_new_client_to_db

--- app.py
+++ app.py
@@ -1,32 +1,47 @@
 from flask import Flask
 from flask import request
+from logging import getLogger
 import json
 import pprint
 import random
 
 from test import test_get_unique_code
-from database import ClientsDatabase
+from database import init_db, get_client, add_new_client_to_db, add_new_message
+
+#Get an instance of a logger
+logger = getLogger(__name__)
 
 app = Flask(__name__)
-db = ClientsDatabase()
 
-@app.route('/', methods=['GET'])
-def add_new_client(unique_identifier):
+@app.route('/', methods=['POST'])
+def add_new_client():
     '''
     the function accepts a request to create a new client with unique code
     '''
-    return db.add_new_client(unique_identifier)
+    r = request.get_json()
+    unique_identifier = r['unique_identifier']
+    add_new_client_to_db(unique_identifier)
+    return '200'
 
 
 @app.route('/message', methods=['POST'])
 def provide_text_message(message, unique_code, unique_identifier):
-    if db.add_new_message(unique_identifier,unique_code,message):
+    if get_client(unique_code, unique_identifier):
+        add_new_message(unique_identifier, unique_code, message)
+        logger.info(f'{message}')
         return "ok"
     else:
         return "An error has occurred"
 
 
-test_get_unique_code(add_new_client)
+def main():
+    print('Start app')
+    #test_get_unique_code(add_new_client)
+    #Connect to database
+    init_db()
+    logger.info('Database create')
+
 
 if __name__ == '__main__':
+    main()
     app.run(host='0.0.0.0', port=8000, debug=True)
--- database.py
+++ database.py
@@ -1,55 +1,79 @@
 import sqlite3
 
+def ensure_connection(function):
+    def inner(*args, **kwargs):
+        with sqlite3.connect('clients.db') as connection:
+            result = function(*args, connection=connection, **kwargs)
+        return result
+    return inner
 
-class ClientsDatabase:
-    def __init__(self, database = "clients.sqlite"):
-        self._db = database
-        with sqlite3.connect(self._db) as connection:
-            cursor = connection.cursor()
-            cursor.execute("""
-            PRAGMA foreign_keys=on;
-            """)
-            connection.commit()
 
-            cursor.execute("""
-            CREATE TABLE IF NOT EXIST messages (
-            id INTEGER PRIMARY KEY,
-            message TEXT NOT NULL
-            );
-            """)
-            connection.commit()
+@ensure_connection
+def init_db(connection):
+    cursor = connection.cursor()
+    cursor.execute("""
+        PRAGMA foreign_keys=on;
+        """)
+    connection.commit()
 
-            cursor.execute("""
-            CREATE TABLE IF NOT EXIST clients (
-            id INTEGER PRIMARY KEY,
-            unique_identifier TEXT NOT NULL,
-            unique_code TEXT NOT NULL,
-            FOREIGN KEY (messages_id) REFERENCES messages(id)
-            );
-            """)
-            connection.commit()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS messages(
+        id INTEGER PRIMARY KEY,
+        message TEXT
+        );
+        """)
+    connection.commit()
 
-    def get_clients(self):
-        '''
-        The method outputs a list of clients
-        '''
-        pass
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS clients(
+        id INTEGER PRIMARY KEY,
+        unique_identifier TEXT NOT NULL,
+        unique_code TEXT NOT NULL,
+        messages_id INTEGER,
+        FOREIGN KEY (messages_id) REFERENCES messages(id)
+        );
+        """)
+    connection.commit()
 
-    def add_new_client(self, unique_identifier):
-        '''
-        The method adds a new client to the database
-        :param unique_identifier: unique client ID
-        :return:unique_code -
-        '''
-        pass
+@ensure_connection
+def get_client(unique_code, unique_identifier, connection) -> bool:
+    '''
+    :param unique_code:
+    :param unique_identifier:
+    :return: true if client exist or false if don't exist
+    '''
+    cursor = connection.cursor()
+    cursor.execute('''
+        SELECT from clients id
+        WHERE (unique_code==unique_code, unique_identifier==unique_identifier)
+        ''')
+    connection.commit()
 
-    def add_new_message(self, unique_identifier, unique_code, message) -> bool:
-        '''
-        The method writes the provided text message to a log file if client code is correct
-        :param unique_identifier:
-        :param unique_code:
-        :param message:
-        :return:
-        '''
-        pass
+
+@ensure_connection
+def add_new_client_to_db(unique_identifier, connection):
+    '''
+    The method adds a new client to the database
+    :param unique_identifier: unique client ID
+    :return:unique_code -
+    '''
+    cursor = connection.cursor()
+    result = cursor.execute("""
+        INSERT INTO clients
+        (unique_identifier, unique_code,)
+        VALUES (?, random());""", (unique_identifier,))
+    connection.commit()
+
+
+def add_new_message(unique_identifier, unique_code, message, connection) -> bool:
+    '''
+    The method writes the provided text message to a log file if client code is correct
+    :param unique_identifier:
+    :param unique_code:
+    :param message:
+    :return:
+    '''
+    pass
+
+
 
