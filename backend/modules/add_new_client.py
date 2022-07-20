7728cfed8356e8b180e7c0e9e2c2dada5f3ca182
LAPTOP-E31FCVM6\sibbr <sibbreakers@gmail.com>
2022-07-21 01:51:43 +0800

    Add request's error handling

--- backend/modules/add_new_client.py
+++ backend/modules/add_new_client.py
@@ -3,6 +3,11 @@ from database.database import add_new_client_to_db
 from .check_unique_identifier import check_unique_identifier_type
 from .generate_unique_code import generate_unique_code
 import json
+import logging.config
+
+# Settings for logging
+logging.config.fileConfig("./logging.ini", disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
 
 add_new_client_bp = Blueprint('add_new_client', __name__)
 
@@ -16,13 +21,18 @@ def add_new_client():
     try:
         data = json.loads(request.data)
         unique_identifier = data['unique_identifier']
-    except ValueError:
-        return {"status": "error", "message": "no json sent"}
-    except KeyError:
+    # except ValueError:
+    #     return {"status": "error", "message": "no json sent"}
+    except KeyError as key_error:
+        logger.error(f'Error: {key_error}')
         return {"status": "error", "message": "key is not correct"}
+    except json.JSONDecodeError as decode_error:
+        logger.error(f'Error: {decode_error}')
+        return {"status": "error", "message": "json is not valid"}
+
     if check_unique_identifier_type(unique_identifier):
         unique_code = generate_unique_code()
         add_new_client_to_db(unique_identifier, unique_code)
-        return {"unique_code": unique_code}
+        return {"status": "200", "unique_code": unique_code}
     else:
-        return {"status": "error", "message": "no string format"}
+        return {"status": "error", "message": "unique identifier has not string format"}
--- backend/provide_text/provide_text_message.py
+++ backend/provide_text/provide_text_message.py
@@ -5,23 +5,31 @@ from flask import Blueprint, request
 from database.database import get_client
 
 # Settings for logging
-#logging.basicConfig(filename='./var/log/messages.log', encoding='utf-8', level=logging.INFO)
+# logging.basicConfig(filename='./var/log/messages.log', encoding='utf-8', level=logging.INFO)
 logging.config.fileConfig("./logging.ini", disable_existing_loggers=False)
 # Get an instance of a specific named logger
 logger = logging.getLogger("message")
+error_logger = logging.getLogger(__name__)
 
 provide_text_message_bp = Blueprint('provide_text_message', __name__)
 
 
 @provide_text_message_bp.route('/message', methods=['POST'])
 def provide_text_message():
-    data = json.loads(request.data)
-    # unique_identifier = data['unique_identifier']
-    unique_code = data['unique_code']
-    unique_identifier = data['unique_identifier']
-    message = data['message']
-    if get_client(unique_code, unique_identifier):
-        logger.info(f'Client {unique_identifier} added new message: {message}')
-        return {"status": "ok"}
-    else:
-        return {"status": "error", "message": "client code does not match client identifier"}
+    try:
+        data = json.loads(request.data)
+        # unique_identifier = data['unique_identifier']
+        unique_code = data['unique_code']
+        unique_identifier = data['unique_identifier']
+        message = data['message']
+        if get_client(unique_code, unique_identifier):
+            logger.info(f'Client {unique_identifier} added new message: {message}')
+            return {"status": "ok"}
+        else:
+            return {"status": "error", "message": "client code does not match client identifier"}
+    except KeyError as key_error:
+        error_logger.error(f'Error: {key_error} This key is not correct')
+        return {"status": "error", "message": "key is not correct"}
+    except json.JSONDecodeError as decode_error:
+        error_logger.error(f'Error: {decode_error}')
+        return {"status": "error", "message": "json is not valid"}
